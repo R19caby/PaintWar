971eb04821853aeba0602d428608058ed663b4eb
r19caby <raphael.caby@imt-atlantique.net>
2020-12-02 11:26:43 +0100

    Ajout de ButtonFactory

---
+++ src/com/paintwar/client/view/components/ButtonFactory.java
@@ -0,0 +1,25 @@
+package com.paintwar.client.view.components;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class ButtonFactory extends JButton {
+	private String cardName;
+	public ButtonFactory(String name, String cardName) {
+		super(name);
+		addActionListener(new RedirectActionListener());
+	}
+	
+	public class RedirectActionListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// Changement dans le card layout de la Frame principale.
+			// accès au cardName
+		}
+		
+	}
+}
