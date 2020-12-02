d3dde9b6b56ea0e947aebc70ab1b7616fc1b6d1d
j19cadio <justine.cadiou@imt-atlantique.net>
2020-12-02 17:47:07 +0100

    first implementation of the collection page (problem with the categories' buttons display)

---
+++ src/com/paintwar/client/view/pages/TestCollection.java
@@ -0,0 +1,16 @@
+package com.paintwar.client.view.pages;
+
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class TestCollection {
+	
+	public static void main(String[] args) {
+		JFrame window = new JFrame();
+		window.setSize(new Dimension(1080, 720));
+		window.add(new Collection());
+		window.setVisible(true);
+	}
+
+}
