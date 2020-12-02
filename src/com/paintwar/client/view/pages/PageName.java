6c6a27742521560d2071b22d07cc36e77620c6d4
r19caby <raphael.caby@imt-atlantique.net>
2020-12-02 13:53:35 +0100

    PageName updated

--- src/com/paintwar/client/view/pages/PageName.java
+++ src/com/paintwar/client/view/pages/PageName.java
@@ -1,8 +1,12 @@
 package com.paintwar.client.view.pages;
 
+
 public enum PageName {
-	CONNEXION_CHOICE("ConnexionChoice"), LOG_IN("LogIn"), SIGN_IN("SignIn"), PARAMETERS("Parameters"), GUEST("Guest");
+	CONNEXION_CHOICE("ConnexionChoice"), LOG_IN("LogIn"), SIGN_IN("SignIn"), PARAMETERS("Parameters"), GUEST("Guest"),
+	HOME("Home"), COLLECTION("Collection"), BATTLEPASS("Battlepass"), SHOP("Shop");
+
 	final String name;
+
 	private PageName(String name) {
 		this.name = name;
 	}
