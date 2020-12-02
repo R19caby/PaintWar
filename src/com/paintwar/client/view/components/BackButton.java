e7efc7ad47e146e6b728e10f0a246e3e6637d180
r19caby <raphael.caby@imt-atlantique.net>
2020-12-02 11:51:44 +0100

    Ajout de buttons dans les components

---
+++ src/com/paintwar/client/view/components/BackButton.java
@@ -0,0 +1,12 @@
+package com.paintwar.client.view.components;
+
+public class BackButton extends ButtonFactory {
+
+	private static final long serialVersionUID = -5498137597461053518L;
+	public BackButton(String page) {
+		super("Retour", page);
+		/*
+		 * Ici les modifs graphiques genre couleur etc
+		 */
+	}
+}
---
+++ src/com/paintwar/client/view/components/ParametersButton.java
@@ -0,0 +1,12 @@
+package com.paintwar.client.view.components;
+
+public class ParametersButton extends ButtonFactory {
+
+	private static final long serialVersionUID = -5498137512421053518L;
+	public ParametersButton() {
+		super("Paramètres", "parametersPage");
+		/*
+		 * Ici les modifs graphiques genre couleur etc
+		 */
+	}
+}
