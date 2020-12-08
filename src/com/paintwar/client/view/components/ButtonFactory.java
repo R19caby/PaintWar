fca3594f4842792c3e4408d0618e131bb33a4be3
Julien Kerlero <julien.kerlero@laposte.net>
2020-12-08 16:18:50 +0100

    Amusez vous <3

--- src/com/paintwar/client/view/components/ButtonFactory.java
+++ src/com/paintwar/client/view/components/ButtonFactory.java
@@ -1,42 +1,125 @@
 package com.paintwar.client.view.components;
 
 import java.awt.CardLayout;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.border.Border;
 
+import com.paintwar.client.logger.Logger;
 import com.paintwar.client.view.MainWindow;
 import com.paintwar.client.view.pages.PageName;
 
-public class ButtonFactory extends RoundButton {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 2910361933495135715L;
-	
-	private String cardName;
-	private MainWindow manager;
-	
-	public ButtonFactory(String name, PageName pageName, MainWindow parent) {
-		super(name);
-		manager = parent;
-		addActionListener(new RedirectActionListener(pageName.toString()));
+/**
+ * This is a button factory.
+ * <p>
+ * {@code JButton newButton = ButtonFactory.getInstance().getButton();}
+ * <p>
+ * Ouais je sais c'est nul désolé je ne comprends pas pourquoi la classe interne
+ * privée CutsomButton et RoundedBorder ne marchent pas dans un contexte
+ * statique.
+ * 
+ * @author jkerl
+ *
+ */
+public class ButtonFactory
+{
+	private ButtonFactory()
+	{
 	}
-	
-	public class RedirectActionListener implements ActionListener {
-		private String pageToReach;
-		
-		public RedirectActionListener(String category) {
-			this.pageToReach = category;
+
+	private static ButtonFactory instance;
+
+	public static ButtonFactory getInstance()
+	{
+		if (instance == null)
+		{
+			instance = new ButtonFactory();
+		}
+		return instance;
+	}
+
+	public JButton getButton(String name, PageName pageName, MainWindow manager)
+	{
+		JButton b = new CustomButton(name, pageName, manager);
+		b.setBorder(new RoundedBorder(10));
+		return b;
+	}
+
+	public JButton getParameterButton(MainWindow manager)
+	{
+		JButton b = new JButton("Failed");
+		try
+		{
+			b = new JButton(new ImageIcon(new URL("https://img.icons8.com/ios/452/settings.png")));
+		} catch (MalformedURLException e)
+		{
+			Logger.print(e);
+		}
+
+		b.setOpaque(false);
+		b.setContentAreaFilled(false);
+		b.setBorderPainted(false);
+		// b.setPreferredSize(new Dimension(30, 30));
+
+		return b;
+	}
+
+	private class CustomButton extends JButton implements ActionListener
+	{
+		private PageName pageName;
+		private MainWindow manager;
+
+		private CustomButton(String name, PageName pageName, MainWindow manager)
+		{
+			super(name);
+			this.pageName = pageName;
+			this.manager = manager;
+
+			addActionListener(this);
 		}
 
 		@Override
-		public void actionPerformed(ActionEvent e) {
+		public void actionPerformed(ActionEvent e)
+		{
 			CardLayout cl = (CardLayout) (manager.getContentContainer().getLayout());
-			cl.show(manager.getContentContainer(), pageToReach);
+			cl.show(manager.getContentContainer(), getName());
 		}
-		
+
+	}
+
+	private class RoundedBorder implements Border
+	{
+		private int radius;
+
+		public RoundedBorder(int radius)
+		{
+			this.radius = radius;
+		}
+
+		@Override
+		public void paintBorder(Component c, Graphics g, int x, int y, int width, int height)
+		{
+			g.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+		}
+
+		@Override
+		public Insets getBorderInsets(Component c)
+		{
+			return new Insets(this.radius + 1, this.radius + 1, this.radius + 2, this.radius);
+		}
+
+		@Override
+		public boolean isBorderOpaque()
+		{
+			return true;
+		}
 	}
 }
--- src/com/paintwar/client/view/components/Header.java
+++ src/com/paintwar/client/view/components/Header.java
@@ -1,47 +1,51 @@
 package com.paintwar.client.view.components;
 
 import java.awt.FlowLayout;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 import com.paintwar.client.view.MainWindow;
 import com.paintwar.client.view.pages.PageName;
 
-public class Header extends JPanel {
-	
+public class Header extends JPanel
+{
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 3670573890343632722L;
-	
-	private ButtonFactory home;
-	private ButtonFactory collection;
-	private ButtonFactory battlepass;
-	private ButtonFactory shop;
-	private ButtonFactory parameters;
-	private ButtonFactory quit;	
-	
-	public Header(MainWindow manager) {
-		
+
+	private JButton home;
+	private JButton collection;
+	private JButton battlepass;
+	private JButton shop;
+	private JButton parameters;
+	private JButton quit;
+
+	public Header(MainWindow manager)
+	{
+
 		this.setLayout(new FlowLayout());
-		
-		home = new ButtonFactory("Home", PageName.HOME, manager);
+
+		home = ButtonFactory.getInstance().getButton("Home", PageName.HOME, manager);
 		this.add(home);
-		
-		collection = new ButtonFactory("Collection", PageName.COLLECTION, manager);
+
+		collection = ButtonFactory.getInstance().getButton("Collection", PageName.COLLECTION, manager);
 		this.add(collection);
-		
-		battlepass = new ButtonFactory("BattlePass", PageName.BATTLEPASS, manager);
+
+		battlepass = ButtonFactory.getInstance().getButton("BattlePass", PageName.BATTLEPASS, manager);
 		this.add(battlepass);
-		
-		shop = new ButtonFactory("Shop", PageName.SHOP, manager);
+
+		shop = ButtonFactory.getInstance().getButton("Shop", PageName.SHOP, manager);
 		this.add(shop);
-		
-		parameters = new ParametersButton(manager);
+
+		parameters = ButtonFactory.getInstance().getParameterButton(manager);
 		this.add(parameters);
-		
-		quit = new ButtonFactory("Quit", PageName.CONNEXION_CHOICE, manager);
+
+		quit = ButtonFactory.getInstance().getButton("Quit", PageName.CONNEXION_CHOICE, manager);
 		this.add(quit);
-		
+
 	}
 
 }
