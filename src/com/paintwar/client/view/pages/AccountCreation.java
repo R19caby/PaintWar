90c63d93aecff8eb50210b69e412b134e9cad511
ThalieB <thalie.borderon@imt-atlantique.net>
2020-12-08 15:32:31 +0100

    Amélioration de l'affichage/masquage des mots de passe + changements dans AccountCreation et LogIn

--- src/com/paintwar/client/view/pages/AccountCreation.java
+++ src/com/paintwar/client/view/pages/AccountCreation.java
@@ -3,12 +3,18 @@ package com.paintwar.client.view.pages;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 import com.paintwar.client.view.MainWindow;
@@ -20,6 +26,7 @@ public class AccountCreation extends JPanel {
 	
 	private static final long serialVersionUID = 6599217595831008888L;
 	private MainWindow manager;
+	private char defaultEchoChar;
 	
 	public AccountCreation(MainWindow parent) {
 		super();
@@ -49,27 +56,44 @@ public class AccountCreation extends JPanel {
 		nickname.setPreferredSize(new Dimension(200, 20));
 		nickname.setMaximumSize(new Dimension(200, 20));
 		nickname.setText("Pseudo");
-		nickname.addMouseListener(new ResetText(nickname, "Pseudo"));
+		nickname.addMouseListener(new ResetSimpleText(nickname, "Pseudo"));
 		JTextField address = new JTextField();
 		dataToEnter.add(address);
 		address.setPreferredSize(new Dimension(200, 20));
 		address.setMaximumSize(new Dimension(200, 20));
 		address.setText("Adresse mail");
-		address.addMouseListener(new ResetText(address, "Adresse mail"));
+		address.addMouseListener(new ResetSimpleText(address, "Adresse mail"));
 		
 		JPanel passwords = new JPanel();
 		dataToEnter.add(passwords);
-		passwords.setLayout(new FlowLayout());
-		JTextField enter = new JTextField();
-		passwords.add(enter);
+		passwords.setMaximumSize(new Dimension(800, 50));
+		passwords.setLayout(new GridLayout(2, 2));
+		JPanel enterP = new JPanel();
+		JPasswordField enter = new JPasswordField();
+		enterP.add(enter);
 		enter.setPreferredSize(new Dimension(200, 20));
 		enter.setText("Entrez le mot de passe");
-		enter.addMouseListener(new ResetText(enter, "Entrez le mot de passe"));
-		JTextField confirm = new JTextField();
-		passwords.add(confirm);
+		defaultEchoChar = enter.getEchoChar();
+		enter.setEchoChar((char) 0);
+		JCheckBox showFirst = new JCheckBox("Afficher le mot de passe");
+		showFirst.addActionListener(new HideText(showFirst, enter, "Entrez le mot de passe"));
+		enter.addMouseListener(new ResetPasswordText(enter, "Entrez le mot de passe", showFirst));
+		
+		JPanel confirmP = new JPanel();
+		JPasswordField confirm = new JPasswordField();
+		confirmP.add(confirm);
 		confirm.setPreferredSize(new Dimension(200, 20));
 		confirm.setText("Confirmez le mot de passe");
-		confirm.addMouseListener(new ResetText(confirm, "Confirmez le mot de passe"));
+		confirm.setEchoChar((char) 0);
+		JCheckBox showSecond = new JCheckBox("Afficher la confirmation");
+		showSecond.addActionListener(new HideText(showSecond, confirm, "Confirmez le mot de passe"));
+		confirm.addMouseListener(new ResetPasswordText(confirm, "Confirmez le mot de passe", showSecond));
+		
+
+		passwords.add(enterP);
+		passwords.add(confirmP);
+		passwords.add(showFirst);
+		passwords.add(showSecond);
 		
 		JPanel south = new JPanel();
 		add(south, BorderLayout.SOUTH);
@@ -79,18 +103,46 @@ public class AccountCreation extends JPanel {
 		
 	}
 	
-	public class ResetText implements MouseListener {
+	
+	
+	public class HideText implements ActionListener {
+		
+		private JCheckBox checkbox;
+		private JPasswordField associatedField;
+		private String printing;
+		
+		public HideText(JCheckBox box, JPasswordField field, String text) {
+			checkbox = box;
+			associatedField = field;
+			printing = text;
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			if (checkbox.isSelected()) {
+				associatedField.setEchoChar((char) 0);
+			} else {
+				if (!associatedField.getText().equals(printing))
+				associatedField.setEchoChar(defaultEchoChar);
+			}
+		}
+		
+	}
+	
+	public class ResetSimpleText implements MouseListener {
 		
 		private JTextField associatedField;
 		private String printing;
 		
-		public ResetText(JTextField field, String text) {
-			this.associatedField = field;
-			this.printing = text;
+		
+		public ResetSimpleText(JTextField field, String text) {
+			associatedField = field;
+			printing = text;
 		}
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
-			associatedField.setText("");
+			if (associatedField.getText().equals("") || associatedField.getText().equals(printing)) {
+				associatedField.setText("");
+			}
 		}
 		@Override
 		public void mouseEntered(MouseEvent arg0) {
@@ -99,6 +151,45 @@ public class AccountCreation extends JPanel {
 		public void mouseExited(MouseEvent arg0) {
 			if (associatedField.getText().equals("")) {
 				this.associatedField.setText(printing);
+			}
+		}
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+		}
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+		}
+	}
+
+	public class ResetPasswordText implements MouseListener {
+		
+		private JPasswordField associatedField;
+		private String printing;
+		private JCheckBox checkbox;
+		
+		public ResetPasswordText(JPasswordField field, String text, JCheckBox box) {
+			associatedField = field;
+			printing = text;
+			checkbox = box;
+		}
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			if (associatedField.getText().equals("") || associatedField.getText().equals(printing)) {
+				associatedField.setText("");
+				associatedField.setEchoChar(defaultEchoChar);
+				if (checkbox.isSelected()) {
+					associatedField.setEchoChar((char) 0);
+				}
+			}
+		}
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+		}
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			if (associatedField.getText().equals("")) {
+				associatedField.setText(printing);
+				associatedField.setEchoChar((char) 0);
 			}
 		}
 		@Override
--- src/com/paintwar/client/view/pages/LogIn.java
+++ src/com/paintwar/client/view/pages/LogIn.java
@@ -3,11 +3,14 @@ package com.paintwar.client.view.pages;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
@@ -21,6 +24,7 @@ public class LogIn extends JPanel {
 
 	private static final long serialVersionUID = 7402238299632129019L;
 	private MainWindow manager;
+	private char defaultEchoChar;
 	
 	public LogIn(MainWindow parent) {
 		super();
@@ -47,16 +51,21 @@ public class LogIn extends JPanel {
 		loginData.setLayout(new BoxLayout(loginData, BoxLayout.PAGE_AXIS));
 		JTextField userID = new JTextField();
 		loginData.add(userID);
-		userID.setPreferredSize(new Dimension(200, 20));
-		userID.setMaximumSize(new Dimension(200, 20));
+		userID.setPreferredSize(new Dimension(300, 20));
+		userID.setMaximumSize(new Dimension(300, 20));
 		userID.setText("Pseudo/Adresse mail");
-		userID.addMouseListener(new ResetText(userID, "Pseudo/Adresse mail"));
+		userID.addMouseListener(new ResetSimpleText(userID, "Pseudo/Adresse mail"));
 		JPasswordField mdp = new JPasswordField();
 		loginData.add(mdp);
-		mdp.setPreferredSize(new Dimension(200, 20));
-		mdp.setMaximumSize(new Dimension(200, 20));
+		mdp.setPreferredSize(new Dimension(300, 20));
+		mdp.setMaximumSize(new Dimension(300, 20));
+		defaultEchoChar = mdp.getEchoChar();
+		mdp.setEchoChar((char) 0);
 		mdp.setText("Mot de passe");
-		mdp.addMouseListener(new ResetText(mdp, "Mot de passe"));
+		JCheckBox showPassword = new JCheckBox("Afficher le mot de passe");
+		loginData.add(showPassword);
+		mdp.addMouseListener(new ResetPasswordText(mdp, "Mot de passe", showPassword));
+		showPassword.addActionListener(new HideText(showPassword, mdp, "Mot de passe"));
 		
 		
 		JButton validate = new JButton("Valider");
@@ -67,43 +76,99 @@ public class LogIn extends JPanel {
 		
 	}
 	
-public class ResetText implements MouseListener {
+	public class HideText implements ActionListener {
+		
+		private JCheckBox checkbox;
+		private JPasswordField associatedField;
+		private String printing;
+		
+		public HideText(JCheckBox box, JPasswordField field, String text) {
+			checkbox = box;
+			associatedField = field;
+			printing = text;
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			if (checkbox.isSelected()) {
+				associatedField.setEchoChar((char) 0);
+			} else {
+				if (!associatedField.getText().equals(printing))
+				associatedField.setEchoChar(defaultEchoChar);
+			}
+		}
+		
+	}
+	
+	public class ResetSimpleText implements MouseListener {
 		
 		private JTextField associatedField;
 		private String printing;
 		
-		public ResetText(JTextField field, String text) {
-			this.associatedField = field;
-			this.printing = text;
+		
+		public ResetSimpleText(JTextField field, String text) {
+			associatedField = field;
+			printing = text;
 		}
-
 		@Override
 		public void mouseClicked(MouseEvent arg0) {
-			associatedField.setText("");
+			if (associatedField.getText().equals("") || associatedField.getText().equals(printing)) {
+				associatedField.setText("");
+			}
 		}
-
 		@Override
 		public void mouseEntered(MouseEvent arg0) {
 		}
-
 		@Override
 		public void mouseExited(MouseEvent arg0) {
 			if (associatedField.getText().equals("")) {
 				this.associatedField.setText(printing);
 			}
-			
 		}
-
 		@Override
 		public void mousePressed(MouseEvent arg0) {
 		}
-
 		@Override
 		public void mouseReleased(MouseEvent arg0) {
 		}
+	}
 
-
-
+	public class ResetPasswordText implements MouseListener {
+		
+		private JPasswordField associatedField;
+		private String printing;
+		private JCheckBox checkbox;
+		
+		public ResetPasswordText(JPasswordField field, String text, JCheckBox box) {
+			associatedField = field;
+			printing = text;
+			checkbox = box;
+		}
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			if (associatedField.getText().equals("") || associatedField.getText().equals(printing)) {
+				associatedField.setText("");
+				associatedField.setEchoChar(defaultEchoChar);
+				if (checkbox.isSelected()) {
+					associatedField.setEchoChar((char) 0);
+				}
+			}
+		}
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+		}
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			if (associatedField.getText().equals("")) {
+				associatedField.setText(printing);
+				associatedField.setEchoChar((char) 0);
+			}
+		}
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+		}
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+		}
 	}
 
 }
